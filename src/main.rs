d5d297470c7822b41a1d09301590118fffc3477a
lucasjstn <lucas7justino@gmail.com>
2023-01-17 16:52:00 -0300

    import examples rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-fn main() {
+#![allow(unused)]
 
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+fn main() {
     println!("Hello, world!");
 }
