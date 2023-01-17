9174ac3c1abfb1fb740a94c33b3f9e0cb30080e6
lucasjstn <lucas7justino@gmail.com>
2023-01-17 17:05:50 -0300

    getting user entry example

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,12 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 fn main() {
-    println!("Hello, world!");
+    print!("What is your name?");
+    let mut name = String::new();
+    let greeting = "Nice to meet you";
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Didn't receive input");
+
+    println!("Hello {}! {}", name.trim_end(), greeting)
 }
