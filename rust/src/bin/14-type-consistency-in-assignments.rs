663abcc026359dadd2d60f944bc6f1d2def6cbca
lucasjstn <lucas7justino@gmail.com>
2023-01-29 18:20:02 -0300

    type consistency

---
+++ rust/src/bin/14-type-consistency-in-assignments.rs
@@ -0,0 +1,15 @@
+fn main() {
+ let mut a = 1;
+ println!("{}", a);
+
+ a = 2;
+ println!(" {}", a);
+
+ a = 3;
+ println!(" {}", a);
+
+ a = 3.14;
+ //this will cause error
+ 
+ println!("{}", a);
+}
